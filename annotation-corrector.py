f099736499135caf79cb371fe1c380d68878f8a5
paoap <41495061+paoap@users.noreply.github.com>
2022-07-03 12:59:00 -0400

    Add files via upload

---
+++ annotation-corrector.py
@@ -0,0 +1,41 @@
+import glob
+import argparse
+import os
+
+parser = argparse.ArgumentParser(prefix_chars="--")
+parser.add_argument("--source", help="specify the directory of the original xml files.", default='DefaultAnnotations', type=str)
+parser.add_argument("--target", help="specify the save location directory of the xml files", default='Corrected-annotations', type=str)
+args = parser.parse_args()
+
+file_list = glob.glob(args.source + "/*.xml")
+
+if not (os.path.isdir('Corrected-annotations')):
+    os.mkdir("Corrected-annotations")
+
+os.closerange
+i=0
+for file in file_list:
+    new_text=[]
+    with open(file,'r') as f:
+        text = f.readlines()
+        for line in text:
+            # change folder
+            if line.find("<folder>Annotations-labelimg")>0:
+                i+=1
+                line="\t<folder>plants</folder>\n"
+            elif line.find("<database>Unknown")>0:
+                line="\t\t<database>plants</database>\n\t\t<annotation>custom</annotation>\n\t\t<image>custom</image>\n"
+            elif line.find("<path>")>0:
+                continue
+            elif line.find("<truncated>1")>0:
+                line="\t\t<truncated>0</truncated>\n"
+            new_text.append(line)
+        f.close
+    
+    x1=file.find("\\")+1
+    save_name=file[x1:]
+    with open(args.target +'/' + save_name,'w') as f:
+        new_text = "".join(new_text)
+        f.write(new_text)
+
+print("{} files edited. Done.".format(i))
---
+++ imagesets-textfile-generator.py
@@ -0,0 +1,54 @@
+import glob
+import argparse
+import os
+import random
+
+parser = argparse.ArgumentParser(prefix_chars="--")
+parser.add_argument("--source", help="specify the directory of the images.", default='JPEGImages', type=str)
+parser.add_argument("--target", help="specify the save location directory of the text files", default='Imagesets/New', type=str)
+args = parser.parse_args()
+
+file_list = glob.glob(args.source + "/*.jpg")
+random.shuffle(file_list)
+
+n_samples = len(file_list)
+print("total_images = {}".format(n_samples))
+
+train_samples = n_samples*0.8
+print("train_images = {}".format(train_samples))
+
+val_samples = n_samples*0.2
+print("val_images = {}".format(val_samples))
+
+train_list=[]
+val_list=[]
+
+i=0
+for f in file_list:
+    i+=1
+    x1=f.find("\\")+1
+    x2=f.find(".")
+    f=f[x1:x2]
+    if i<=train_samples:
+        train_list.append(f)
+    else:
+        val_list.append(f)
+
+train_list = '\n'.join(train_list)
+val_list = '\n'.join(val_list)
+trainval_list = train_list+ '\n' + val_list
+
+if not (os.path.isdir('Imagesets/New')):
+    os.mkdir("Imagesets/New")
+
+with open(args.target+"/train.txt","w") as output:
+    output.write(str(train_list))
+
+with open(args.target+"/val.txt","w") as output:
+    output.write(str(val_list))
+
+with open(args.target+"/test.txt","w") as output:
+    output.write(str(val_list))
+
+with open(args.target+"/trainval.txt","w") as output:
+    output.write(str(trainval_list))
